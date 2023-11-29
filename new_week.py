3d969f8507d3f709f959c4d67f694141f6fb9aaf
will-siv <wsimkinsiv@gmail.com>
2023-11-28 19:16:58 -0500

    started parsing 'tree'

--- new_week.py
+++ new_week.py
@@ -3,6 +3,7 @@
 # tui that replaces the week closed schedule
 import json
 import sys
+import datetime
 
 def printSchedule(d):
   # prints closed time schedule
@@ -15,10 +16,58 @@ def printSchedule(d):
     else:
       print(s[0:-2])
 
+def printHelp():
+  msg = """
+  usage: [time1][+/-][time2]
+  a time can be represented like 'xx:xx' or 'xx'
+  '-': from time1 to time2
+  '+': from time1 for time2 duration
+  """
+  print(msg)
+
+def parseStmt(stmt):
+  # [xx[:xx]][+/-][xx[:xx]]
+  # returns 2-list of times, or False if not valid
+  ret = []
+  if "+" in stmt:
+    plus = True
+    times = stmt.split("+")
+  else if "-" in stmt:
+    plus = False
+    times = stmt.split("-")
+  else:
+    print('error: missing operator')
+    return False
+
+  if len(times) != 2:
+    print('error: invalid operation')
+    return False
+
+  for t in times:
+    # make sure time is in correct format, standardize, and add to list
+    hourmin = t.split(":")
+    if len(hourmin) == 1:
+      hourmin.append("00")
+    if len(hourmin[0]) != 2 or len(hourmin[1]) != 2:
+      # error for invalid time t
+
 def addNew(d, day):
   # guides user through adding a new closed time to the schedule
-  print("not implemented yet, but will be soon!")
+  print("Type the times to close (24 hour)")
+  print("Type ? for operations")
+  try:
+    stmt = input()
+  except EOFError:
+    return
 
+  # handle operations
+  if stmt == "?":
+    printHelp()
+    addNew(d, day)
+    return
+  out = parseStmt(stmt)
+  if not out:
+    print("Invalid input")
 
 def main():
   file = "weekly_closed_times.json"
